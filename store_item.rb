502d5961b9e35492d2db6e8606552881b393ae3f
Robert Rossol <shyguyrbr1@gmail.com>
2017-02-27 19:56:59 -0600

    creates file

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+dog1={:breed=> "pug", :color=> "black and white", :temperment=> "lazy"}
+dog2={:breed=> "australian shepherd", :color=> "brown", :temperment=> "friendly"}
+dog3={:breed=> "poodle", :color=> "white", :temperment=> "smug"}
+
